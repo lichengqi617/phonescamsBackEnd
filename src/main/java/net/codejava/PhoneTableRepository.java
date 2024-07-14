58b53a1691d17d6c231a20c5b1034dfe4be69a8e
lichengqi617 <59191361+lichengqi617@users.noreply.github.com>
2024-07-14 01:31:44 -0700

    update table name (#8)

--- src/main/java/net/codejava/PhoneTableRepository.java
+++ src/main/java/net/codejava/PhoneTableRepository.java
@@ -9,6 +9,6 @@ import java.util.Optional;
 
 public interface PhoneTableRepository extends JpaRepository<PhoneTable, Integer> {
     @Query(nativeQuery = true, 
-        value = "select p from PhoneTable where PhoneTable.CountryID = :countryID and PhoneTable.PhoneNumber = :phoneNumber")
+        value = "select p from phone_table PhoneTable where PhoneTable.CountryID = :countryID and PhoneTable.PhoneNumber = :phoneNumber")
     Optional<PhoneTable> findByCountryIDAndPhoneNumber(@Param("countryID") String countryID, @Param("phoneNumber") String phoneNumber);
 }
